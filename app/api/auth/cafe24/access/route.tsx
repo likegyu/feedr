1f08589ef0c2afd28f95fea598af66243680e572
gyu <gyu@likegyu.com>
2025-01-08 05:12:43 +0900

    scope 인코딩 수정

--- app/api/auth/cafe24/access/route.tsx
+++ app/api/auth/cafe24/access/route.tsx
@@ -6,7 +6,8 @@ export async function GET(req: NextRequest) {
   const redirectUri = encodeURIComponent(process.env.CAFE24_REDIRECT_URI!);
   const mallId = req.nextUrl.searchParams.get('mall_id');
   const scope = 'mall.read_store,mall.write_application,mall.read_application,mall.write_design,mall.read_design'; // 필요한 scope를 설정
-  const authorizationUrl = `https://${mallId}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=${clientId}&state=${mallId}&redirect_uri=${redirectUri}&scope=${scope}`;
+  const encodedScope = encodeURIComponent(scope);  // 인코딩된 스코프 값
+  const authorizationUrl = `https://${mallId}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=${clientId}&state=${mallId}&redirect_uri=${redirectUri}&scope=${encodedScope}`;
   if (!clientId || !redirectUri || !mallId) {
     return NextResponse.json({ error: 'Missing required parameters' }, { status: 400 });
   }
