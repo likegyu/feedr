121504717df12a62ef704d96869622324d763a36
gyu <gyu@likegyu.com>
2025-01-08 05:16:42 +0900

    scope 변수 삭제

--- app/api/auth/cafe24/access/route.tsx
+++ app/api/auth/cafe24/access/route.tsx
@@ -5,9 +5,7 @@ export async function GET(req: NextRequest) {
   const clientId = process.env.CAFE24_CLIENT_ID!;
   const redirectUri = encodeURIComponent(process.env.CAFE24_REDIRECT_URI!);
   const mallId = req.nextUrl.searchParams.get('mall_id');
-  const scope = 'mall.read_store,mall.write_application,mall.read_application,mall.write_design,mall.read_design'; // 필요한 scope를 설정
-  const encodedScope = encodeURIComponent(scope);  // 인코딩된 스코프 값
-  const authorizationUrl = `https://${mallId}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=${clientId}&state=${mallId}&redirect_uri=${redirectUri}&scope=${encodedScope}`;
+  const authorizationUrl = `https://${mallId}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=${clientId}&state=${mallId}&redirect_uri=${redirectUri}&scope=mall.read_store,mall.read_application,mall.write_application,mall.write_design,mall.read_design`;
   if (!clientId || !redirectUri || !mallId) {
     return NextResponse.json({ error: 'Missing required parameters' }, { status: 400 });
   }
