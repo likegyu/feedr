f575d8f7bf933e029ef4da0d6767b688ae5dcaab
gyu <gyu@likegyu.com>
2025-01-08 18:13:01 +0900

    토큰 타입 정의

--- lib/db.tsx
+++ lib/db.tsx
@@ -1,13 +1,26 @@
 import { Pool, QueryResult } from 'pg';
 
+// 토큰 데이터 구조에 맞는 타입 정의
+type Token = {
+  access_token: string;
+  expires_at: number;
+  refresh_token: string;
+  refresh_token_expires_at: number;
+  client_id: string;
+  mall_id: string;
+  user_id: string;
+  scopes: string;
+  issued_at: number;
+};
+
 const pool = new Pool({
   connectionString: process.env.NEON_DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, 
+    rejectUnauthorized: false,
   },
 });
 
 // `db.query` 반환 값에 대해 구체적인 타입 지정
 export const db = {
-  query: async (text: string, params?: any[]): Promise<QueryResult<any>> => pool.query(text, params),
+  query: async (text: string, params?: any[]): Promise<QueryResult<Token>> => pool.query(text, params),
 };
