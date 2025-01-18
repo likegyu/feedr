82cd6fe2312154a95c92b496cb0751e8e654a244
gyu <gyu@likegyu.com>
2025-01-18 20:26:27 +0900

    노치디자인

--- app/settings/mobile/page.tsx
+++ app/settings/mobile/page.tsx
@@ -246,7 +246,7 @@ const MobileFeedSettings = () => {
         <div className="flex justify-center">
           <div className="relative w-[320px] h-[640px] bg-black rounded-[3rem] p-4 shadow-xl">
             {/* 노치 디자인 */}
-            <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-40 h-6 bg-black rounded-b-3xl flex items-center justify-center">
+            <div className="absolute top-1.5 left-1/2 z-11 transform -translate-x-1/2 w-20 h-6 bg-black rounded-b-3xl flex items-center justify-center">
               <div className="w-20 h-4 bg-black rounded-lg"></div>
             </div>
             {/* 모바일 스크린 */}
